28cad7e38d9207c033a81e68ff947ef6b0f18d3b
Sonat Karan <sonat.karan@infosfer.com>
2016-04-02 19:23:24 +0300

    WIP

---
+++ Assets/ActionController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActionController : MonoBehaviour
+{
+
+    private bool isInputEnabled;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    private void CharacterController()
+    {
+        if (isInputEnabled)
+        {
+            
+        }
+    }
+}
--- Assets/Scripts/MiniGame1.cs
+++ Assets/Scripts/MiniGame1.cs
@@ -1,20 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MiniGame1 : MonoBehaviour, ILevel {
+public class MiniGame1 : MonoBehaviour, ILevel
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	    CoreLoop();
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CoreLoop();
+    }
+
     public void CoreLoop()
     {
-        throw new System.NotImplementedException();
+        
     }
 
     public void StartLevel()
